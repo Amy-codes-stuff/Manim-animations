94fd42b964c6dc0e06618c1810b9b99e04e5a8f7
Amy-codes-stuff <tiwarekaramisha12@gmail.com>
2025-06-10 18:39:47 +0530

    testing

---
+++ main.py
@@ -0,0 +1,19 @@
+def main():
+    print("Hello from manimations!")
+
+
+if __name__ == "__main__":
+    main()
+from manim import *
+
+class ShapeExample(Scene):
+    def construct(self):
+        circle = Circle().set_color(BLUE)
+        square = Square().set_color(RED).shift(RIGHT * 2)
+
+        self.play(Create(circle))
+        self.wait(1)
+        self.play(Transform(circle, square))
+        self.wait(1)
+        self.play(FadeOut(circle))
+
